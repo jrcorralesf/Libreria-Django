e89fb853bfc82e614b1e09c442dfbdb9aadcce3a
Julian Ricardo Corrales Fierro <jrcorralesf@unal.edu.co>
2021-06-01 16:07:48 -0500

    Creación de urls

--- applications/author/views.py
+++ applications/author/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import AuthorModel
+
+class AuthorListView(ListView):
+    model = AuthorModel
+    context_object_name = 'list_author'
+    template_name = "author/author_list.html"
+
